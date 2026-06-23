fb81900308fda09ead71f623deae623d143bb4c3
Aman Ranjan <2300033544@kluniversity.in>
2026-06-23 15:46:16 +0530

    Time: 1 ms (83.05%), Space: 42.7 MB (72.95%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -3,32 +3,18 @@ import java.util.*;
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String[] words = s.split(" ");
-        
+
         if (pattern.length() != words.length) {
             return false;
         }
 
-        HashMap<Character, String> charToWord = new HashMap<>();
-        HashMap<String, Character> wordToChar = new HashMap<>();
+        HashMap<Object, Integer> map = new HashMap<>();
 
         for (int i = 0; i < pattern.length(); i++) {
-            char ch = pattern.charAt(i);
-            String word = words[i];
-
-            if (charToWord.containsKey(ch)) {
-                if (!charToWord.get(ch).equals(word)) {
-                    return false;
-                }
-            } else {
-                charToWord.put(ch, word);
-            }
-
-            if (wordToChar.containsKey(word)) {
-                if (wordToChar.get(word) != ch) {
-                    return false;
-                }
-            } else {
-                wordToChar.put(word, ch);
+            if (!Objects.equals(
+                    map.put(pattern.charAt(i), i),
+                    map.put(words[i], i))) {
+                return false;
             }
         }
 
