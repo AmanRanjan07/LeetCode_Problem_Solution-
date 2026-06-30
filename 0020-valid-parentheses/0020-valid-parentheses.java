afb64c7d2503bec3f1554197f2865beea6a5911c
Aman Ranjan <2300033544@kluniversity.in>
2026-07-01 00:05:23 +0530

    Time: 3 ms (87.38%), Space: 43.3 MB (57.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,19 +2,21 @@ import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
-
+        // here you may use the stack approach to solve the problem in that to push the 
+        // brackets into the stack and check weather it is avaliable on the top or not
+        // if the character is present on the top of the stack return true else false.
         Stack<Character> stack = new Stack<>();
 
         for (char ch : s.toCharArray()) {
 
             if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
+                stack.push(ch);   //Pushing into the stack...
             } else {
 
                 if (stack.isEmpty()) {
-                    return false;
+                    return false;  // if the stack is empty means return false
                 }
-
+// checking the condition into this make sure about the element should be present in order..
                 char top = stack.pop();
 
                 if (ch == ')' && top != '(') {
