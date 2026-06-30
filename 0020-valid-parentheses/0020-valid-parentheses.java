91dfdd2e65920e86e8b4d59f5e9c23d9f7b3189d
Aman Ranjan <2300033544@kluniversity.in>
2026-07-01 00:01:01 +0530

    Time: 3 ms (87.38%), Space: 43.2 MB (72.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,36 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
+
         Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(c == '(' || c == '{' || c == '['){
-                stack.push(c);
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
             } else {
-                if(stack.isEmpty()) return false;
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
                 char top = stack.pop();
 
-                if(c == ')' && top != '(') return false;
-                if(c == '}' && top != '{') return false;
-                if(c == ']' && top != '[') return false;
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
             }
         }
-        return stack.isEmpty();
 
+        return stack.isEmpty();
     }
 }
