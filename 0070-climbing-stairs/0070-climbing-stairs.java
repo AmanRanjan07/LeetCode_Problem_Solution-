5fc215b272c1ba971ff1be07056f39d7dfe20607
Aman Ranjan <2300033544@kluniversity.in>
2026-07-06 15:02:15 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.49%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        int n1,n2,n3;
+        if(n<=2){
+            return n;
+        }  
+        
+            n1=1;
+            n2=2;
+        
+        for(int i=3;i<=n;i++){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+        }
+        return n2;
+    }
+}
