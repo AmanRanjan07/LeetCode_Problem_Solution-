4d73dc999c361244c2378b9136bfe2f9a07a3cf3
Aman Ranjan <2300033544@kluniversity.in>
2026-07-15 11:46:15 +0530

    Time: 0 ms (100%), Space: 61.2 MB (70.82%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,40 +1,70 @@
+// // class Solution {
+// //     public void rotate(int[] nums, int k) {
+
+// //         int n = nums.length;
+// //         k = k % n;
+
+// //         for (int i = 0; i < k; i++) {
+
+// //             // Save last element
+// //             int last = nums[n - 1];
+
+// //             // Shift all elements to the right
+// //             for (int j = n - 1; j > 0; j--) {
+// //                 nums[j] = nums[j - 1];
+// //             }
+
+// //             // Put last element at first position
+// //             nums[0] = last;
+// //         }
+// //     }
+// // }
+
 // class Solution {
 //     public void rotate(int[] nums, int k) {
 
 //         int n = nums.length;
 //         k = k % n;
 
-//         for (int i = 0; i < k; i++) {
+//         int[] temp = new int[n];
 
-//             // Save last element
-//             int last = nums[n - 1];
+//         for (int i = 0; i < n; i++) {
+//             temp[(i + k) % n] = nums[i];
+//         }
 
-//             // Shift all elements to the right
-//             for (int j = n - 1; j > 0; j--) {
-//                 nums[j] = nums[j - 1];
-//             }
-
-//             // Put last element at first position
-//             nums[0] = last;
+//         // Copy back
+//         for (int i = 0; i < n; i++) {
+//             nums[i] = temp[i];
 //         }
 //     }
 // }
 
+
 class Solution {
+
     public void rotate(int[] nums, int k) {
 
         int n = nums.length;
+
         k = k % n;
 
-        int[] temp = new int[n];
+        reverse(nums, 0, n - 1);
 
-        for (int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
-        }
+        reverse(nums, 0, k - 1);
 
-        // Copy back
-        for (int i = 0; i < n; i++) {
-            nums[i] = temp[i];
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+
+        while (left < right) {
+
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
         }
     }
 }
