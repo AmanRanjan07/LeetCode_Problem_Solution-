095b672a613f9ce1052fedcf2a4715754dafafe5
Aman Ranjan <2300033544@kluniversity.in>
2026-07-24 11:49:31 +0530

    Time: 7 ms (10.12%), Space: 46.7 MB (72.88%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,25 +1,39 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode(int x) {
+//  *         val = x;
+//  *         next = null;
+//  *     }
+//  * }
+//  */
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while(fast != null && fast.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if(slow == fast){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+// here this one is the HashSet Approach
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast){
+        HashSet <ListNode> set = new HashSet<>();
+        while(head != null){
+            if(set.contains(head))
                 return true;
-            }
+            set.add(head);
+            head = head.next;
         }
         return false;
+
     }
 }
