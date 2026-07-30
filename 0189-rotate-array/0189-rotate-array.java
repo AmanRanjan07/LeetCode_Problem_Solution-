93837c352e6791723f78477117a16cf03acbc17a
Aman Ranjan <2300033544@kluniversity.in>
2026-07-30 23:33:50 +0530

    Time: 0 ms (100%), Space: 61 MB (93.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,70 +1,20 @@
-// // class Solution {
-// //     public void rotate(int[] nums, int k) {
-
-// //         int n = nums.length;
-// //         k = k % n;
-
-// //         for (int i = 0; i < k; i++) {
-
-// //             // Save last element
-// //             int last = nums[n - 1];
-
-// //             // Shift all elements to the right
-// //             for (int j = n - 1; j > 0; j--) {
-// //                 nums[j] = nums[j - 1];
-// //             }
-
-// //             // Put last element at first position
-// //             nums[0] = last;
-// //         }
-// //     }
-// // }
-
-// class Solution {
-//     public void rotate(int[] nums, int k) {
-
-//         int n = nums.length;
-//         k = k % n;
-
-//         int[] temp = new int[n];
-
-//         for (int i = 0; i < n; i++) {
-//             temp[(i + k) % n] = nums[i];
-//         }
-
-//         // Copy back
-//         for (int i = 0; i < n; i++) {
-//             nums[i] = temp[i];
-//         }
-//     }
-// }
-
-
 class Solution {
-
     public void rotate(int[] nums, int k) {
-
         int n = nums.length;
-
         k = k % n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
 
-        reverse(nums, 0, n - 1);
-
-        reverse(nums, 0, k - 1);
-
-        reverse(nums, k, n - 1);
     }
+    public static void reverse(int []arr,int i,int j){
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
 
-    private void reverse(int[] nums, int left, int right) {
-
-        while (left < right) {
-
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-
-            left++;
-            right--;
         }
     }
 }
