89be21f6fd6263da90c9dd2cb73467bf92aaa833
Aman Ranjan <2300033544@kluniversity.in>
2026-08-12 21:23:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,13 +1,13 @@
 class Solution {
     public int reverse(int x) {
         int rev = 0;
-        while(x!=0){
-            int rem = x%10;
-             if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+        while(x !=0){
+            int dig = x%10;
+            if(rev>Integer.MAX_VALUE||rev<Integer.MIN_VALUE){
                 return 0;
             }
-            rev = (rev*10) + rem;
-            x = x / 10;
+            rev = (rev * 10)+ dig;
+            x/=10;
         }
         return rev;
     }
