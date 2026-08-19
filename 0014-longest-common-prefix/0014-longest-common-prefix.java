e722fb0013caca5f75a8b9a907af8670bdde2810
Aman Ranjan <2300033544@kluniversity.in>
2026-08-19 11:14:55 +0530

    Time: 1 ms (61.09%), Space: 43 MB (92.56%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -2,14 +2,22 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         // // If no strings
         // if (strs == null || strs.length == 0) return "";
-
+        if(strs == null || strs.length == 0) return "";
         // // Use first string as reference
         // String first = strs[0];
-
+        String first = strs[0];
         // // Go through each character in the first string
         // for (int i = 0; i < first.length(); i++) {
         //     char c = first.charAt(i); // current char in first string
-
+        for(int i=0;i<first.length();i++){
+            char c = first.charAt(i);
+            for(int j=1;j<strs.length;j++){
+                if(i >= strs[j].length() || strs[j].charAt(i) != c){
+                    return first.substring(0,i);
+                }
+            }
+        }
+        return first;
         //     // Compare this char with same index in all other strings
         //     for (int j = 1; j < strs.length; j++) {
         //         // If this string is too short or mismatch found
@@ -24,17 +32,17 @@ class Solution {
         // return first;
 
 
-        if(strs == null || strs.length == 0) return "";
-        String first = strs[0];
-        for(int i=0;i<first.length();i++){
-            char c = first.charAt(i);
-            for(int j = 1;j<strs.length;j++){
-                if(i>=strs[j].length() || strs[j].charAt(i) != c){
-                    return first.substring(0,i);
-                }
-            }
-        }
-        return first;
+        // if(strs == null || strs.length == 0) return "";
+        // String first = strs[0];
+        // for(int i=0;i<first.length();i++){
+        //     char c = first.charAt(i);
+        //     for(int j = 1;j<strs.length;j++){
+        //         if(i>=strs[j].length() || strs[j].charAt(i) != c){
+        //             return first.substring(0,i);
+        //         }
+        //     }
+        // }
+        // return first;
     }
 }
     /*0123456  012345  01234
