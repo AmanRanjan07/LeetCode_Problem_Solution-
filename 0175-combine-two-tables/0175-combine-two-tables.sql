f1fe9a2c732c6abc4c28e449cc50fba36031d8a6
Aman Ranjan <2300033544@kluniversity.in>
2026-08-27 21:52:33 +0530

    Time: 412 ms (81%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
-Select p.firstName, p.lastName, a.city,a.state
-from Person p left join Address a
-on p.personId = a.personId
-
 
 -- Why LEFT JOIN? The question says:
 -- Return every person from the Person table, even if that person does not have an address.
+
+Select p.firstName,p.lastName,a.city,a.state
+from Person p left Join Address a
+on p.personId = a.personId
+-- order by firstName;
