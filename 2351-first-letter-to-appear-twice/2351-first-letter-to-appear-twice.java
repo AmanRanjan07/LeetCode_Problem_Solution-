226aef017d8c33581c884506eca8bcf800d271fa
Aman Ranjan <2300033544@kluniversity.in>
2026-08-28 22:53:39 +0530

    Time: 0 ms (100%), Space: 43 MB (17.05%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,15 +1,13 @@
 class Solution {
     public char repeatedCharacter(String s) {
-
-        HashSet<Character> set = new HashSet<>();
+        HashSet<Character> set  = new HashSet<>();
         for(int i=0;i<s.length();i++){
-                char ch = s.charAt(i);
-                if(set.contains(ch)){
-                    return ch;
-                }
-                set.add(ch);
+            char ch = s.charAt(i);
+            if(set.contains(ch)){
+                return ch;
             }
-        
+            set.add(ch);
+        }
         return ' ';
     }
 }
