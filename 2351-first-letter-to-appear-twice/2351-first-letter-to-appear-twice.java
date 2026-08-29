485ba38aaa843c9dcc1905e7bcf489705a30a43b
Aman Ranjan <2300033544@kluniversity.in>
2026-08-29 21:37:31 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.93%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,6 +1,6 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        HashSet<Character> set  = new HashSet<>();
+        HashSet<Character> set = new HashSet<>();
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             if(set.contains(ch)){
