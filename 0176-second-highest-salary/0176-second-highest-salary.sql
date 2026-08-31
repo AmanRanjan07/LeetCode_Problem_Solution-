2ad35733912067338dc2eae09bf39ba4ac04149d
Aman Ranjan <2300033544@kluniversity.in>
2026-08-31 22:03:36 +0530

    Time: 249 ms (98.46%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,7 @@
-Select(
-    select distinct salary
-    from Employee
-    order by salary desc
-    limit 1 offset 1
+
+
+select (select distinct salary
+from Employee
+order by salary desc
+limit 1 offset 1
 ) as SecondHighestSalary;
