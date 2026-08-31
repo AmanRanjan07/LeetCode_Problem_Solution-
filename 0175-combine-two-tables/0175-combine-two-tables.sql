be6ecaa49fc912e5225a520172180547c0e27129
Aman Ranjan <2300033544@kluniversity.in>
2026-08-31 22:01:58 +0530

    Time: 398 ms (89.31%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -2,7 +2,8 @@
 -- Why LEFT JOIN? The question says:
 -- Return every person from the Person table, even if that person does not have an address.
 
-Select p.firstName,p.lastName,a.city,a.state
-from Person p left Join Address a
+
+
+select p.firstName,p.lastName,a.city,a.state
+from Person p left join Address a 
 on p.personId = a.personId
--- order by firstName;
