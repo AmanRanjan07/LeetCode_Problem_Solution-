d45335ae83eb275d65c3d1d5b5d999d4fa9fae44
Aman Ranjan <2300033544@kluniversity.in>
2026-09-03 22:00:19 +0530

    Time: 883 ms (5.17%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,5 +1,16 @@
-Select 
-    score,
-    DENSE_RANK() OVER (ORDER BY score DESC) as 'rank'
-From Scores
-ORDER BY score DESC;
+-- Select 
+--     score,
+--     DENSE_RANK() OVER (ORDER BY score DESC) as 'rank'
+-- From Scores
+-- ORDER BY score DESC;
+
+
+SELECT 
+    s1.score,
+    (
+        SELECT COUNT(DISTINCT s2.score)
+        from Scores s2
+        WHERE s2.score >= s1.score
+    ) AS 'rank'
+FROM Scores s1
+ORDER BY s1.score DESC;
