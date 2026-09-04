747a5d101071ca486946e9e5d917399d80c329fd
Aman Ranjan <2300033544@kluniversity.in>
2026-09-04 11:17:37 +0530

    Time: 1 ms (99.89%), Space: 56 MB (6.73%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int votecount  = 0;
+
+        for(int num : nums){
+            if(votecount == 0){
+                candidate = num;
+            }
+            if(num == candidate){
+                votecount++;
+            }
+            else{
+                votecount--;
+            }
+        }
+        return candidate;
+    }
+}
