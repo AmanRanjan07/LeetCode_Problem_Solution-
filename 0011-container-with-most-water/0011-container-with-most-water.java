be1fadfafaf3b2581414e7cbb8c0f5f7d00c679a
Aman Ranjan <2300033544@kluniversity.in>
2026-09-08 15:15:01 +0530

    Time: 5 ms (83.01%), Space: 77.3 MB (49.21%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,28 +1,28 @@
 class Solution {
     public int maxArea(int[] height) {
 
-        int left = 0;
-        int right = height.length - 1;
+        int left = 0; // take the left most height here 
+        int right = height.length - 1;// take the right most height of the container
 
-        int maxArea = 0;
+        int maxArea = 0; // Inlisized the the maxArea as 0..
 
-        while (left < right) {
+        while (left < right) {  // check until l < r
+ 
+            int width = right - left;  // length between the two container
 
-            int width = right - left;
+            int currHeight = Math.min(height[left], height[right]);  
+    // find the height which is the min height among the two container
+            int area = width * currHeight; // calculate the area between the two container
 
-            int currHeight = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, area); // take maximum values between them
 
-            int area = width * currHeight;
-
-            maxArea = Math.max(maxArea, area);
-
-            if (height[left] < height[right]) {
-                left++;
+            if (height[left] < height[right]) {// check height between both the container here
+                left++;  // do left ++ if the left side is small here
             } else {
-                right--;
+                right--;  // otherwise do right ++ 
             }
         }
 
-        return maxArea;
+        return maxArea;  // Final answer, maximun =  width * maximum height .... 
     }
 }
